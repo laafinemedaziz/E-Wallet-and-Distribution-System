114f50f28395096a8e178cce482016f327c4b43c
Med Aziz Laafine <laafine.med.aziz@gmail.com>
2025-03-06 15:21:54 +0100

    6th commit

---
+++ custom/addons/clevory_auth/controllers/authentication_controller.py
@@ -0,0 +1,44 @@
+import json
+import logging
+import operator
+
+from werkzeug.urls import url_encode
+from odoo.addons.web.controllers.session import Session
+import odoo
+import odoo.modules.registry
+from odoo import http
+from odoo.modules import module
+from odoo.exceptions import AccessError, UserError, AccessDenied
+from odoo.http import request, Response
+from odoo.exceptions import ValidationError
+
+
+
+_logger = logging.getLogger(__name__)
+
+class authController(Session):
+    
+    @http.route('/web/session/authenticate', type='json', auth="none")
+    def authenticate(self):
+
+        #Retrieving request body and validating content
+        requestBody = request.httprequest.get_json()
+        if 'login' not in requestBody or 'password' not in requestBody:
+            raise ValidationError("Missing important credentials to authenticate")
+
+        login = requestBody.get('login')
+        password = requestBody.get('password')
+        db = request.db
+
+        #Check if user is valid (Email verification constraint)
+        user = request.env['res.users'].sudo().search([('login','=',login)],limit=1)
+        if not user:
+            raise AccessError('Invalid credentials')
+        elif user.status == 'invalid':
+            raise AccessError("User is not valid.")
+        
+        #Calling the odoo's authentication method with the right parameters
+        session_infos = super().authenticate(db, login, password)
+        
+        return {'uid':request.session.uid,
+            'message':"user is successfully authenticated."}
