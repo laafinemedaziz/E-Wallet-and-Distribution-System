f2268939498e28b3ed5338b8c7b41439a8e7d7af
Med Aziz Laafine <laafine.med.aziz@gmail.com>
2025-03-26 18:17:12 +0100

    16th commit

--- custom/addons/e_wallet_manager/controllers/wallet_controller.py
+++ custom/addons/e_wallet_manager/controllers/wallet_controller.py
@@ -15,6 +15,7 @@ class WalletController(http.Controller):
     @http.route('/api/getEmpsWallet', type='http', auth='user', methods=['GET'], csrf=False)
     def getEmpsWallets(self):
         user = request.env.user
+        #Change it to check whether he has a certain group or not instead of chekcing type
         if user.type != 'hr':
             raise AccessDenied(f"Prohibited action for user type: {user.type}")
         else:
